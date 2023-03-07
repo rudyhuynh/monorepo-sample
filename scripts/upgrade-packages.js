0c1a09689ad3aadb7d5839632852d43ba2269330
Rudy Huynh <rudy.huynh286@gmail.com>
2023-03-07 20:45:22 +0700

    add scripts/upgrade-packages.js

---
+++ scripts/upgrade-packages.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+const { execSync } = require("child_process");
+
+const workspaces = JSON.parse(execSync(`npm query .workspace`).toString());
+
+workspaces
+  .filter(({ name }) => name.startsWith("@rd/"))
+  .forEach((workspace) => {
+    const newVersion = execSync(
+      `npm version patch --workspace ${workspace.name} --no-workspaces-update`
+    )
+      .toString()
+      .split("\n")[1]
+      .slice(1);
+
+    console.log("workspace", workspace.name);
+    console.log("\t old version", workspace.version);
+    console.log("\t new version", newVersion);
+
+    const userWorkspaces = workspaces.filter(
+      ({ dependencies }) => dependencies?.[workspace.name]
+    );
+
+    userWorkspaces.forEach((userWorkspace) => {
+      if (userWorkspace.dependencies?.[workspace.name] !== "*") {
+        execSync(
+          `npm pkg set dependencies.${workspace.name}=${newVersion} --workspace ${userWorkspace.name}`
+        );
+
+        console.log("\t updated user workspace", userWorkspace.name);
+      }
+    });
+  });
+
+execSync("npm install");
